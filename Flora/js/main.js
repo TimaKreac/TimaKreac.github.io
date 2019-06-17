621710ef0c603c856d308b36b468e1da7020af1f
TimaKreac <tkreac@gmail.com>
2019-06-17 23:06:21 +0600

    w

--- Flora/js/main.js
+++ Flora/js/main.js
@@ -29,13 +29,6 @@ $(document).ready(function () {
                     slidesToScroll: 1,
                 }
             },
-            {
-                breakpoint: 500,
-                settings: {
-                    prevArrow: `<svg xmlns="http://www.w3.org/2000/svg" id="arrow-left" fill="#c3c3c3"  width="0" height="84" viewBox="0 0 32 84"><g><g><path d="M30 0h2L2 42l30 42h-2L0 42z"/></g></g></svg>`,
-                    nextArrow: `<svg xmlns="http://www.w3.org/2000/svg" id="arrow-right" fill="#c3c3c3"  transform="rotate(180)" width="0" height="84" viewBox="0 0 32 84"><g><g><path d="M30 0h2L2 42l30 42h-2L0 42z"/></g></g></svg>`,
-                }
-            }
 
         ]
 
@@ -50,18 +43,6 @@ $(document).ready(function () {
         prevArrow: `<svg xmlns="http://www.w3.org/2000/svg" id="arrow-left2" fill="#c3c3c3"  width="32" height="84" viewBox="0 0 32 84"><g><g><path d="M30 0h2L2 42l30 42h-2L0 42z"/></g></g></svg>`,
 
         nextArrow: `<svg xmlns="http://www.w3.org/2000/svg" id="arrow-right2" fill="#c3c3c3"  transform="rotate(180)" width="32" height="84" viewBox="0 0 32 84"><g><g><path d="M30 0h2L2 42l30 42h-2L0 42z"/></g></g></svg>`,
-
-        responsive: [
-
-            {
-                breakpoint: 500,
-                settings: {
-                    prevArrow: `<svg xmlns="http://www.w3.org/2000/svg" id="arrow-left" fill="#c3c3c3"  width="0" height="84" viewBox="0 0 32 84"><g><g><path d="M30 0h2L2 42l30 42h-2L0 42z"/></g></g></svg>`,
-                    nextArrow: `<svg xmlns="http://www.w3.org/2000/svg" id="arrow-right" fill="#c3c3c3"  transform="rotate(180)" width="0" height="84" viewBox="0 0 32 84"><g><g><path d="M30 0h2L2 42l30 42h-2L0 42z"/></g></g></svg>`,
-                }
-            }
-
-        ]
     });
 
     $('.info-slider').slick({
@@ -75,17 +56,7 @@ $(document).ready(function () {
         prevArrow: `<svg xmlns="http://www.w3.org/2000/svg" id="arrow-left3" fill="#c3c3c3"  width="32" height="84" viewBox="0 0 32 84"><g><g><path d="M30 0h2L2 42l30 42h-2L0 42z"/></g></g></svg>`,
 
         nextArrow: `<svg xmlns="http://www.w3.org/2000/svg" id="arrow-right3" fill="#c3c3c3"  transform="rotate(180)" width="32" height="84" viewBox="0 0 32 84"><g><g><path d="M30 0h2L2 42l30 42h-2L0 42z"/></g></g></svg>`,
-        responsive: [
 
-            {
-                breakpoint: 500,
-                settings: {
-                    prevArrow: `<svg xmlns="http://www.w3.org/2000/svg" id="arrow-left" fill="#c3c3c3"  width="0" height="84" viewBox="0 0 32 84"><g><g><path d="M30 0h2L2 42l30 42h-2L0 42z"/></g></g></svg>`,
-                    nextArrow: `<svg xmlns="http://www.w3.org/2000/svg" id="arrow-right" fill="#c3c3c3"  transform="rotate(180)" width="0" height="84" viewBox="0 0 32 84"><g><g><path d="M30 0h2L2 42l30 42h-2L0 42z"/></g></g></svg>`,
-                }
-            }
-
-        ]
 
     });
 
@@ -93,5 +64,54 @@ $(document).ready(function () {
     $(function () {
         $(".features-form_input").mask("+7 (999) 999-99-99");
     });
+
+
+    /********  modal *******/
+    // $(".modal").each(function () {
+    //     $(this).wrap('<div class="overlay"></div>')
+    // });
+
+    // $(".open-modal").on('click', function (e) {
+    //     e.preventDefault();
+    //     e.stopImmediatePropagation;
+
+    //     var $this = $(this),
+    //         modal = $($this).data("modal");
+
+    //     $(modal).parents(".overlay").addClass("open");
+    //     setTimeout(function () {
+    //         $(modal).addClass("open");
+    //     }, 350);
+
+    //     $(document).on('click', function (e) {
+    //         var target = $(e.target);
+
+    //         if ($(target).hasClass("overlay")) {
+    //             $(target).find(".modal").each(function () {
+    //                 $(this).removeClass("open");
+    //             });
+    //             setTimeout(function () {
+    //                 $(target).removeClass("open");
+    //             }, 350);
+    //         }
+
+    //     });
+
+    // });
+
+    // $(".close-modal").on('click', function (e) {
+    //     e.preventDefault();
+    //     e.stopImmediatePropagation;
+
+    //     var $this = $(this),
+    //         modal = $($this).data("modal");
+
+    //     $(modal).removeClass("open");
+    //     setTimeout(function () {
+    //         $(modal).parents(".overlay").removeClass("open");
+    //     }, 350);
+
+    // });
+
 
 });
