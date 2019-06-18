0c4a91410573caf7ce1d2fc0406558b624b7f7f2
TimaKreac <tkreac@gmail.com>
2019-06-18 14:36:21 +0600

    qw

--- Flora/js/main.js
+++ Flora/js/main.js
@@ -1,13 +1,5 @@
 $(document).ready(function () {
-    $('a[href^="#"]').on('click', function (e) {
-        var target = this.hash,
-            $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 1000, 'swing', function () {
-            window.location.hash = target;
-        });
-    });
+
 
     $('.partners-slider').slick({
         slidesToShow: 4,
